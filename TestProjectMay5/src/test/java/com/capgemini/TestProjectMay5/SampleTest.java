8927ef7a07dcf325cc1748b6b890dd3bd43ede83
santoshprakashsingh <santoshprakash@gmail.com>
2020-05-05 12:50:27 +0530

    Added class

---
+++ TestProjectMay5/src/test/java/com/capgemini/TestProjectMay5/SampleTest.java
@@ -0,0 +1,12 @@
+package com.capgemini.TestProjectMay5;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Welcome to Git hub");
+
+	}
+
+}
